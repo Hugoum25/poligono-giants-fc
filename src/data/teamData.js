58e91f844b001042d17a3aaabcd11f6761ea0971
Hugoum25 <hugoumgi38@gmail.com>
2026-08-19 01:23:07 +0200

    Actualizar datos oficiales de partidos y goles para las 3 leyendas del club v470

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -430,7 +430,7 @@ try {
     });
 } catch (e) {}
 
-const LEGENDS_STORAGE_KEY = 'fc_hub_legends_v3';
+const LEGENDS_STORAGE_KEY = 'fc_hub_legends_v4';
 
 // Cargar estado inicial desde localStorage
 try {
